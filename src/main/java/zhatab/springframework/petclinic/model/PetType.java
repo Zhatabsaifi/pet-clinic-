ccddf3c246704804946978f6d77ffff5ba852477
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-05-17 00:17:21 +0530

    Adding POJOs. Closes #2.

--- src/main/java/zhatab/springframework/petclinic/model/PetType.java
+++ src/main/java/zhatab/springframework/petclinic/model/PetType.java
@@ -1,13 +1,5 @@
 package zhatab.springframework.petclinic.model;
 
 public class PetType {
-   private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
