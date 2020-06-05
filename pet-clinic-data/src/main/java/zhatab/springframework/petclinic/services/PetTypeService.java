c67177903f244eb218544fd4654cb248756a85f9
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-05 23:26:04 +0530

    added PetType map service . Closes #30.

---
+++ pet-clinic-data/src/main/java/zhatab/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package zhatab.springframework.petclinic.services;
+
+import zhatab.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
