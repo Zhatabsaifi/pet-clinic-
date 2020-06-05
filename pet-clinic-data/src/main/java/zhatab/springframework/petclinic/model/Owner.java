ce7bbfb2241423084204b55c752cde0e54fd872c
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-05 22:54:32 +0530

    add visits , update owner with pet relationship. Closes #27

--- pet-clinic-data/src/main/java/zhatab/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/zhatab/springframework/petclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package zhatab.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
 
+    Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
