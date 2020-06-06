4b768d5add29f7c8eafb570d7950fd14cc2eae13
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-06 22:51:35 +0530

    add speciality map service . Closes #32.

---
+++ pet-clinic-data/src/main/java/zhatab/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package zhatab.springframework.petclinic.services;
+
+import zhatab.springframework.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+}
---
+++ pet-clinic-data/src/main/java/zhatab/springframework/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package zhatab.springframework.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import zhatab.springframework.petclinic.model.Speciality;
+import zhatab.springframework.petclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
