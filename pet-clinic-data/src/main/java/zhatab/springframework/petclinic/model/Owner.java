5e412d2f39e4baf1617751a22aa9d276e07a0633
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-12 18:11:47 +0530

    convert owner to JPA entity. Closes #37.

--- pet-clinic-data/src/main/java/zhatab/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/zhatab/springframework/petclinic/model/Owner.java
@@ -1,12 +1,24 @@
 package zhatab.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
-    private  String address,city,telephone;
-    private Set<Pet> pets = new HashSet<>();   // good to assign at initalize time // reduce the chance of exception whe you not insert anything and try to get function
+    @Column(name = "address")
+    private  String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
