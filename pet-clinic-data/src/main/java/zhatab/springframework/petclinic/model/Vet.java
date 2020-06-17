025ee202dfefcbe88952d5f25ed237f881094420
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-17 22:59:16 +0530

    added project lombok to project. Closes #52.

--- pet-clinic-data/src/main/java/zhatab/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/zhatab/springframework/petclinic/model/Vet.java
@@ -1,9 +1,17 @@
 package zhatab.springframework.petclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -13,11 +21,4 @@ public class Vet extends Person{
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
    private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
